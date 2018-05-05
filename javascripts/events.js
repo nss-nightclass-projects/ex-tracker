fe12203cdfe6301b37ee9558bcea08c4d5624240
Anna Dolan <anna.dolan115@gmail.com>
2018-05-05 13:34:35 -0500

    added classes to cards and click events consoling

---
+++ javascripts/events.js
@@ -0,0 +1,11 @@
+// const filter = () => {
+//   const morning = $('.')
+// };
+
+// const bindEvents = () => {
+//   $('.btn').on('click', (e) => {
+//     console.log('e', e);
+//   });
+// };
+
+// module.exports = bindEvents;
