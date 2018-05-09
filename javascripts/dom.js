98dbcff0446cfda0a4f7adc0ccef826f4dde6906
Anna Dolan <anna.dolan115@gmail.com>
2018-05-09 05:13:55 -0400

    wip

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -11,4 +11,20 @@ const writeLocations = (locations) => {
   return strang;
 };
 
-module.exports = writeLocations;
+const writeEx = (ex) => {
+  let strang = '';
+  for (let i = 0; i < ex.length; i++) {
+    strang += `<div class=" col-md-3 thumbnail card">`;
+    strang +=   `<div class="caption">`;
+    strang +=     `<h3 id="thumbnail-label">${location.Name}</h3>`;
+    strang +=     `<p class="caption">${location.LocationAddress}</p>`;
+    strang +=   `</div>`;
+    strang += `</div>`;
+  };
+  return strang;
+};
+
+module.exports = {
+  writeLocations,
+  writeEx,
+};
